616f6fa4a51e23002135686f440ac2a6ba2322d7
Howard Chen <howardxchen@gmail.com>
2014-04-05 17:40:56 -0700

    Add Login Form

--- DoubtMe.js
+++ DoubtMe.js
@@ -2,8 +2,35 @@ if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to DoubtMe.";
   };
+Template.login.events({
 
-  Template.hello.events({
+    'submit #login-form' : function(e, t){
+      e.preventDefault();
+      // retrieve the input field values
+      var email = t.find('#login-email').value
+        , password = t.find('#login-password').value;
+      console.log("call");
+
+        // Trim and validate your fields here.... 
+
+        // If validation passes, supply the appropriate fields to the
+        // Meteor.loginWithPassword() function.
+        Meteor.loginWithPassword(email, password, function(err){
+        if (err) {
+            console.log("Error");
+        }
+          // The user might not have been found, or their passwword
+          // could be incorrect. Inform the user that their
+          // login attempt has failed. 
+        else {
+            console.log("Success");
+        }
+          // The user has been logged in.
+      });
+         return false; 
+      }
+  });
+  Template.login.events({
     'click input': function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
@@ -17,3 +44,5 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+
